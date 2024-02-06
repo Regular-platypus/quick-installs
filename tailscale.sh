0073a4809cf51f54ac85543066278eca8adf0a10
Regular-platypus <118298840+Regular-platypus@users.noreply.github.com>
2024-02-06 23:15:30 +0530

    Update tailscale.sh

--- tailscale.sh
+++ tailscale.sh
@@ -1,4 +1,23 @@
 #!/bin/bash
+
+# Download and execute Tailscale installer script using curl
 curl -fsSL https://tailscale.com/install.sh | sh
-sudo tailscaled --state=tailscaled.state &
-sudo tailscale up
+
+
+#fix network for exit nodes
+echo 'net.ipv4.ip_forward = 1' | sudo tee -a /etc/sysctl.d/99-tailscale.conf
+echo 'net.ipv6.conf.all.forwarding = 1' | sudo tee -a /etc/sysctl.d/99-tailscale.conf
+sudo sysctl -p /etc/sysctl.d/99-tailscale.conf
+
+echo 'net.ipv4.ip_forward = 1' | sudo tee -a /etc/sysctl.conf
+echo 'net.ipv6.conf.all.forwarding = 1' | sudo tee -a /etc/sysctl.conf
+sudo sysctl -p /etc/sysctl.conf
+
+firewall-cmd --permanent --add-masquerade
+
+
+# Run tailscaled in the background
+sudo tailscaled &
+
+# Run tailsccale with ssh and exit node
+sudo tailscale up --ssh --advertise-exit-node
